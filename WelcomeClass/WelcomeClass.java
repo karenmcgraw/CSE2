b64e2e0bae607705616165ccdd2d81260b5e8742
karenmcgraw <kem217@lehigh.edu>
2015-01-29 02:24:42 +0000

    updated WelcomeClass

---
+++ WelcomeClass/WelcomeClass.java
@@ -0,0 +1,22 @@
+//
+//Karen McGraw
+//Homework 1
+//Welcome Java Program
+//
+
+public class WelcomeClass{
+    
+    public static void main(String[] args) {
+        
+        System.out.println("-----------");
+        System.out.println("| WELCOME |");
+        System.out.println("-----------");
+        System.out.println("  ^  ^  ^  ^  ^  ^");
+        System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\");
+        System.out.println("<-K--E--M--2--1--7->");
+        System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+        System.out.println("  v  v  v  v  v  v");
+        System.out.println("I'm a member of the Lehigh Women's Rowing team. I have been a rower since I was 15.");
+
+    }
+}
