ac59b5c9f62432adfd73d47e9cd7222e3265b2be
karenmcgraw <kem217@lehigh.edu>
2015-05-01 00:06:20 +0000

    updated Factorials

--- lab08/Factorials.java
+++ lab08/Factorials.java
@@ -3,35 +3,31 @@
 //Lab 8
 //Create a program that calculates super factorials
 import java.util.Scanner;
+
 public class Factorials{
-    public static void main(String[] args){//first method takes input and does simple factorial
-        Scanner myScanner;
-        myScanner = new Scanner(System.in);
-        int input;
-        int a = 1;
-        int factorial = 1;
-        System.out.println("Enter an integer: ");
-        if(myScanner.hasNextInt()){
-            input = myScanner.nextInt();
-            while(a<=input){
-                factorial *= a;
-                a++;
-            }
-        }
-        else{
-           System.out.println("Invalid input");
-           return;
-        }
-    }//close first method
-    public static int SuperFactorial(int key){//do super factorial in second method
-        int mul = 1;
-        for(int i =1; i<=key; i++){
-            mul *= i;
-        }
-        System.out.println(mul);
-        return mul;
+public static void main (String [] args){
+             
+    Scanner myScanner = new Scanner(System.in);  
+    System.out.print("Enter an positive interger: ");
+    int input = 0;
+    input = myScanner.nextInt();
+    int sum = 0;
+    int i = 0;
+    for(i=1; i <= input; i++){
+    sum += factorial(i);}
+    print(sum);
+    }       
+    
+    public static int factorial(int input){
+    int fact = 1;
+    int j;
+    for(j = 1; j <= input ; j++){
+        fact = fact * j;
     }
-    public static void print(int num){
-        System.out.println("The super factorial is equal to" + num);
+    return fact;
     }
+            public static void print(int sum){
+            System.out.println("The super factorial is "+sum);
+        
+        }
 }
